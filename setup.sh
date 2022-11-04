0970a7b75abe34f4b6a3a8ef425c58680b51d73b
Shams E Shifat <shamseshifat@gmail.com>
2022-11-04 23:43:55 +0600

    refactored. relinked

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/bash
 
+function mklink () {
+	ln -nfs ./$1/ ~/.config/$1
+}
+
 
 # alcritty
-mkdir -p ~/.config/alacritty
-ln alacritty/* ~/.config/alacritty
+mklink "alacritty"
+
+# rofi
+mklink "rofi"
+
+# ranger
+mklink "ranger"
