dab38d2cd0ab67b7bd0661bb9296bd4a5f453726
Shams E Shifat <shamseshifat@gmail.com>
2023-03-20 02:44:01 +0600

    NVIM: keymaps to comment

--- nvim/lua/user/comment.lua
+++ nvim/lua/user/comment.lua
@@ -29,5 +29,36 @@ comment.setup {
       location = location,
     }
   end,
+   ---LHS of toggle mappings in NORMAL mode
+    toggler = {
+        ---Line-comment toggle keymap
+        line = '<leader>cc',
+        ---Block-comment toggle keymap
+        block = '<leader>CC',
+    },
+   ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+    opleader = {
+        ---Line-comment keymap
+        line = '<leader>c',
+        ---Block-comment keymap
+        block = '<leader>C',
+    },
+    ---LHS of extra mappings
+    extra = {
+        ---Add comment on the line above
+        above = '<leader>cO',
+        ---Add comment on the line below
+        below = '<leader>co',
+        ---Add comment at the end of line
+        eol = '<leader>cA',
+    },
+    ---Enable keybindings
+    ---NOTE: If given `false` then the plugin won't create any mappings
+    mappings = {
+        ---Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+        basic = true,
+        ---Extra mapping; `gco`, `gcO`, `gcA`
+        extra = true,
+    },
 }
 
