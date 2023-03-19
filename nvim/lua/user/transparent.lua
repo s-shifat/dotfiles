4de9a23139e1537207cc41314d37ee9419ead678
Shams E Shifat <shamseshifat@gmail.com>
2023-03-20 02:44:43 +0600

    NVIM: removed keymaps to transparent.lua

--- nvim/lua/user/transparent.lua
+++ nvim/lua/user/transparent.lua
@@ -1,16 +1,21 @@
 require("transparent").setup({
   enable = false, -- boolean: enable transparent
+  groups = { -- table: default groups
+    'Normal', 'NormalNC', 'Comment', 'Constant', 'Special', 'Identifier',
+    'Statement', 'PreProc', 'Type', 'Underlined', 'Todo', 'String', 'Function',
+    'Conditional', 'Repeat', 'Operator', 'Structure', 'LineNr', 'NonText',
+    'SignColumn', 'CursorLineNr', 'EndOfBuffer',
+  },
   extra_groups = { -- table/string: additional groups that should be cleared
     -- In particular, when you set it to 'all', that means all available groups
 
     -- example of akinsho/nvim-bufferline.lua
-    -- "BufferLineTabClose",
-    -- "BufferlineBufferSelected",
-    -- "BufferLineFill",
-    -- "BufferLineBackground",
-    -- "BufferLineSeparator",
-    -- "BufferLineIndicatorSelected",
+    "BufferLineTabClose",
+    "BufferlineBufferSelected",
+    "BufferLineFill",
+    "BufferLineBackground",
+    "BufferLineSeparator",
+    "BufferLineIndicatorSelected",
   },
-  exclude = {}, -- table: groups you don't want to clear
-  ignore_linked_group = true, -- boolean: don't clear a group that links to another group
+  exclude_groups = {}, -- table: groups you don't want to clear
 })
