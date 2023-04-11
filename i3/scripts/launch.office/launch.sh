ca50f817a0a239e9aa2c903c328f9ab0d945d89d
Shams E Shifat <shamseshifat@gmail.com>
2023-04-12 04:53:26 +0600

    i3: custom scripts for launching office environment

---
+++ i3/scripts/launch.office/launch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+i3-msg "workspace number 1"
+i3-msg "exec --no-startup-id /home/shifat/.config/i3/scripts/launch.office/w1"
+sleep 2
+
+i3-msg "workspace number 2"
+i3-msg "exec --no-startup-id /home/shifat/.config/i3/scripts/launch.office/w2"
+sleep 2
+
+i3-msg "workspace number 3"
+i3-msg "exec --no-startup-id /home/shifat/.config/i3/scripts/launch.office/w3"
+sleep 2
+
+i3-msg "workspace number 4"
+i3-msg "exec --no-startup-id /home/shifat/.config/i3/scripts/launch.office/w4"
+sleep 2
