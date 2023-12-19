355303fb26a6a2978b679f15bb525cafc48dc4c0
Shams E Shifat <shamseshifat@gmail.com>
2023-12-20 00:54:28 +0600

    lvim update

--- lvim/lua/user/user_plugins.lua
+++ lvim/lua/user/user_plugins.lua
@@ -44,6 +44,31 @@ lvim.plugins = {
   -- vim tmux navigator
   {
     "christoomey/vim-tmux-navigator"
-  }
+  },
+  --  [markdown markmap mindmap]
+  --  https://github.com/Zeioth/markmap.nvim
+  --
+  --
+  {
+    "Zeioth/markmap.nvim",
+    build = "yarn global add markmap-cli",
+    cmd = { "MarkmapOpen", "MarkmapSave", "MarkmapWatch", "MarkmapWatchStop" },
+    opts = {
+      html_output = "/tmp/markmap.html", -- (default) Setting a empty string "" here means: [Current buffer path].html
+      hide_toolbar = false, -- (default)
+      grace_period = 3600000 -- (default) Stops markmap watch after 60 minutes. Set it to 0 to disable the grace_period.
+    },
+    config = function(_, opts) require("markmap").setup(opts) end
+  },
+
+  -- A nice color picker and highlighter
+  {
+    "uga-rosa/ccc.nvim"
+  },
+
+  -- Latex equation display
+  {
+    'jbyuki/nabla.nvim'
+  },
 
 }
