d62955cb434900cc930d4819780faa9aa8f74b5d
Shams E Shifat <shamseshifat@gmail.com>
2025-03-06 20:47:40 -0600

    improved search for telescope

--- lvim/.config/lvim/lua/user/general/options.lua
+++ lvim/.config/lvim/lua/user/general/options.lua
@@ -14,7 +14,7 @@ vim.opt.hlsearch = true                         -- highlight all matches on prev
 vim.opt.ignorecase = true                       -- ignore case in search patterns
 vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim
 vim.opt.pumheight = 10                          -- pop up menu height
-vim.opt.showmode = true                         -- display things like -- INSERT -- 
+vim.opt.showmode = true                         -- display things like -- INSERT --
 vim.opt.showtabline = 2                         -- always show tabs
 vim.opt.smartcase = true                        -- smart case
 vim.opt.smartindent = true                      -- make indenting smarter again
@@ -22,8 +22,8 @@ vim.opt.splitbelow = true                       -- force all horizontal splits t
 vim.opt.splitright = true                       -- force all vertical splits to go to the right of current window
 vim.opt.swapfile = false                        -- no swapfile
 vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
-vim.opt.timeoutlen = 800                       -- time to wait for a mapped sequence to complete (in milliseconds)
-vim.opt.undofile = false                         -- enable persistent undo? Nooooooooo!
+vim.opt.timeoutlen = 800                        -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.undofile = false                        -- enable persistent undo? Nooooooooo!
 vim.opt.updatetime = 300                        -- faster completion (4000ms default)
 vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
 vim.opt.expandtab = true                        -- convert tabs to spaces
@@ -33,13 +33,14 @@ vim.opt.cursorline = true                       -- highlight the current line
 vim.opt.number = true                           -- set numbered lines
 vim.opt.relativenumber = true                   -- set relative numbered lines
 vim.opt.numberwidth = 2                         -- set number column width to 2 {default 4}
-vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
+vim.opt.signcolumn =
+"yes"                                           -- always show the sign column, otherwise it would shift the text each time
 vim.opt.wrap = false                            -- display lines as one long line
-vim.opt.scrolloff = 12                           -- is one of my fav
+vim.opt.scrolloff = 12                          -- is one of my fav
 vim.opt.sidescrolloff = 8
 --vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
-vim.opt.spelllang = "en_us"                     -- Spell checking to english US
-vim.opt.spell = false                           -- Spell checking enabled
+vim.opt.spelllang = "en_us" -- Spell checking to english US
+vim.opt.spell = false       -- Spell checking enabled
 vim.opt.shortmess:append "c"
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
@@ -79,3 +80,20 @@ lvim.builtin.project.patterns = {
   -- "main.py", "venv",
 }
 
+
+
+-- Telescope search specific
+lvim.builtin.telescope.defaults.file_ignore_patterns = {
+  -- python-specific
+  "./node%_modules/*",
+  "./.git/*",
+  "lazy%-lock.json",
+  -- python-specific
+  "./poetry.lock",
+  "./ipynb%_checkpoints/*",
+  "*.ipynb",
+  -- LaTeX specific
+  "main%.aux", "main%.log", "main%.out", "main%.toc", "main%.bbl",
+  "main%.blg", "main%.lof", "main%.lot", "main%.nav", "main%.snm",
+  "main%.fls", "main%.fdb_latexmk", "main%.synctex%.gz"
+}
