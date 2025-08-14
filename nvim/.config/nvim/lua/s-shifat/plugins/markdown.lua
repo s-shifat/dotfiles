baeaab2833c5602d33dc4def74f1ee2b663446b2
Shams E Shifat <shamseshifat@gmail.com>
2025-08-14 18:18:55 -0500

    nvim

---
+++ nvim/.config/nvim/lua/s-shifat/plugins/markdown.lua
@@ -0,0 +1,12 @@
+
+-- install with yarn or npm
+-- https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && npm install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+}
