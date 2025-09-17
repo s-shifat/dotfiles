02241fa74452acd1f83af9c59ee1af161ad37f4b
Shams E Shifat <shamseshifat@gmail.com>
2025-09-17 17:21:18 -0500

    added ocr screenshot

--- lvim/.config/lvim/lua/user/user_plugins.lua
+++ lvim/.config/lvim/lua/user/user_plugins.lua
@@ -285,6 +285,27 @@ lvim.plugins = {
     end,
   },
 
+  -- tmux stuff
+  {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+  },
+
+
   -- nvim surround
   {
     "kylechui/nvim-surround",
