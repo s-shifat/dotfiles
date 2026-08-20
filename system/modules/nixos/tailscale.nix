905fcf93c65eee76c0364e5f0f813a3e24b884df
Shams E Shifat <shamseshifat@gmail.com>
2026-08-20 16:25:01 -0500

    added tailscale

--- system/modules/nixos/tailscale.nix
+++ system/modules/nixos/tailscale.nix
@@ -1,3 +1,9 @@
+# FIX IT:
+# Current Status:
+# At fresh startup, after building, reboot then manually run `sudo systemctl restart tailscaled`
+# Then: `sudo tailscale up`
+# Then: accept from tailscale admin
+
 { ... }:
 
 {
