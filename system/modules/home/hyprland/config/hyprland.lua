9556b30d5d65594edf4479f68fffbdae69b041b2
Shams E Shifat <shamseshifat@gmail.com>
2026-08-25 23:48:38 -0500

    increase keyboard response

--- system/modules/home/hyprland/config/hyprland.lua
+++ system/modules/home/hyprland/config/hyprland.lua
@@ -260,6 +260,9 @@ hl.config({
         kb_options = "",
         kb_rules   = "",
 
+        repeat_rate  = 30,
+        repeat_delay = 350,
+
         follow_mouse = 1,
 
         sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
